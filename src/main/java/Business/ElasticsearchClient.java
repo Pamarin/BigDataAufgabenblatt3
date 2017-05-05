f84c41c8415a669af24d4d1a5211deb5e4d4ab7d
Jean-Luc Burot <jburot@gmail.com>
2017-05-05 16:50:57 +0200

    Created Elasticsearch client.

---
+++ src/main/java/Business/ElasticsearchClient.java
@@ -0,0 +1,65 @@
+package Business;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+/**
+ *
+ * @author Jean-Luc Burot
+ * @since 2017-05-05
+ */
+public class ElasticsearchClient {
+    private String host;
+    private int port;
+    private TransportClient client;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+    
+    public ElasticsearchClient(String host, int port) throws Exception {
+        this.host = host;
+        this.port = port;
+        
+        openConnection();
+    }
+    
+    public void openConnection() throws UnknownHostException, Exception {
+        System.out.println("Open connection to " + this.host + ":" + this.port + ".");
+        
+        if(this.client == null) {
+            // Connection startup.
+            this.client = new PreBuiltTransportClient(Settings.EMPTY)
+                                         .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(this.host), this.port));
+        } else {
+            throw new Exception("Connection still open.");
+        }
+    }
+    
+    public void closeConnection() throws Exception {
+        System.out.println("Close connection to " + this.host + ":" + this.port + ".");
+        
+        if(this.client != null) {
+            // Connection shutdown.
+            this.client.close();
+        } else {
+            throw new Exception("No connection to close.");
+        }
+    }
+}

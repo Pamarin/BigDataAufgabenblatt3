3d4caa596c2b2f5c1c7a424ca1949de046c52861
Jean-Luc Burot <jburot@gmail.com>
2017-05-06 12:21:26 +0200

    Restructured methods in Main for more efficient handling.

--- src/main/java/com/glasses/programmieraufgabe3/Main.java
+++ src/main/java/com/glasses/programmieraufgabe3/Main.java
@@ -40,28 +40,31 @@ public class Main {
             // Get the command from the command line.
             String command = args[0];
             
+            // Connect to Elasticsearch node.
+            ElasticsearchClient client = new ElasticsearchClient(HOST, PORT);
+
             // Start the appriate part.
             switch(args[0]) {
                 case INDEX_DOCUMENTS:
-                    indexDocuments();
+                    indexDocuments(client);
                     break;
                 case PROCESS_QUERIES:
-                    processQueries();
+                    processQueries(client);
                     break;
                 default:
                     System.out.println("Fehler: '" + command + "' ist kein gültiger Befehl.");
             }
+            
+            // Close connection.
+            client.closeConnection();
         } else {
             System.out.println("Fehler: Parameter Befehl fehlt: " + INDEX_DOCUMENTS + " oder " + PROCESS_QUERIES);
         }
     }
     
-    private static void indexDocuments() throws Exception {
+    private static void indexDocuments(ElasticsearchClient client) throws Exception {
         System.out.println("Dokumente indexieren.");
         
-        // Connect to Elasticsearch node.
-        ElasticsearchClient client = new ElasticsearchClient(HOST, PORT);
-
         // Access file.
         FileReader reader = new FileReader(FILE_DOCUMENTS);
         // Read file.
@@ -76,29 +79,19 @@ public class Main {
             // Index document.
             client.index(document.toJSON(), "documents", "document", document.getId());
         }
-
-        // Close connection.
-        client.closeConnection();
     }
     
-    private static void processQueries() throws IOException, Exception {
-        System.out.println("Queries verarbeiten.");
+    private static ArrayList<Query> loadQueries() throws IOException {
+        System.out.println("Load queries from file.");
         
-        // Lists for queries and judgements.
+        // List for queries.
         ArrayList<Query> queries = new ArrayList<>();
-        ArrayList<Judgement> judgements = new ArrayList<>();
         
         // Access queries file.
         FileReader readerQueries = new FileReader((FILE_QUERIES));
         // Read queries file.
         readerQueries.read(false);
         System.out.println("Gefundene Queries: " + readerQueries.getLines().size());
-        
-        // Access judgements file.
-        FileReader readerJudgements = new FileReader((FILE_JUDGEMENTS));
-        // Read judgements file.
-        readerJudgements.read(false);
-        System.out.println("Gefundene Judgements: " + readerJudgements.getLines().size());
         
         // Fill list with queries.
         for(String line : readerQueries.getLines()) {
@@ -109,23 +102,47 @@ public class Main {
             queries.add(query);
         }
         
+        return queries;
+    }
+    
+    private static ArrayList<Judgement> loadJudgements() throws IOException {
+        System.out.println("Load judgements from file.");
+        
+        // List for judgements.
+        ArrayList<Judgement> judgements = new ArrayList<>();
+        // Access judgements file.
+        FileReader readerJudgements = new FileReader((FILE_JUDGEMENTS));
+        // Read judgements file.
+        readerJudgements.read(false);
+        System.out.println("Gefundene Judgements: " + readerJudgements.getLines().size());
+        
         // Fill list with judgements.
         // Remove non-relevant judgements.
         for(String line : readerJudgements.getLines()) {
             // Parse from tabulator-separated line to Judgements class.
             Judgement judgement = Judgement.parse(line);
             
+            /*
             // Add to judgements list, if relevant.
             if(judgement.isRelevance()) {
                 // Add to judgments list.
                 judgements.add(judgement);
             }
+            */
+            
+            // Add to judgments list.
+            judgements.add(judgement);
         }
         
-        System.out.println("Relevante Judgements: " + judgements.size());
+        return judgements;
+    }
+    
+    private static void processQueries(ElasticsearchClient client) throws IOException, Exception {
+        System.out.println("Queries verarbeiten.");
         
-        // Connect to Elasticsearch node.
-        ElasticsearchClient client = new ElasticsearchClient(HOST, PORT);
+        // Load queries and judgements.
+        ArrayList<Query> queries = loadQueries();
+        ArrayList<Judgement> judgements = loadJudgements();
         
         // Define search terms.
         String searchTerms = "International Organized Crime";
@@ -139,8 +156,5 @@ public class Main {
         // Search in Elasticsearch.
         SearchResponse response = client.search("documents", "document", "title", searchTerms, relevantIds);
         System.out.println(response);
-        
-        // Close connection.
-        client.closeConnection();
     }
 }
